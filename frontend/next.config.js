d8f69cbe2be6b6a7a071a98cb75e6532d15a55de
Harry Collin <10602138+harrycollin@users.noreply.github.com>
2023-02-24 22:10:34 +0000

    Update next.config.js
    Update security headers for routes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,4 +14,18 @@ module.exports = withTM({
 
     return config;
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          }
+        ],
+      },
+    ]
+  },
 })
